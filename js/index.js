8039d83798ea781af4c9213438df7aa27024da3d
wairol <viktorpalyuha@gmail.com>
2019-11-15 17:38:51 +0000

    Sample Code

---
+++ js/index.js
@@ -0,0 +1,67 @@
+document.querySelector('.page-loaded').innerText = new Date().toLocaleTimeString();
+
+document.querySelector('.get-ajax-html').addEventListener('click', getAjaxHtml);
+
+function getAjaxHtml() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.html-placeholder').innerHTML = xhr.responseText;
+        }
+    }
+    xhr.open('get', 'client-data.html', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-html').addEventListener('click', fetchHtml);
+
+function fetchHtml() {
+    fetch('client-data.html')
+        .then( response => response.text() )
+        .then( html =>  document.querySelector('.html-placeholder').innerHTML = html );
+}
+
+document.querySelector('.get-ajax-json').addEventListener('click', getAjaxJson);
+
+function getAjaxJson() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const clientData = JSON.parse(xhr.responseText);
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        }
+    }
+    xhr.open('get', 'client-data.json', true);
+    xhr.send();
+}
+
+document.querySelector('.fetch-json').addEventListener('click', fetchJson);
+
+function fetchJson() {
+    fetch('client-data.json')
+        .then( response => response.json() )
+        .then( clientData =>  {
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        } );
+}
+
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
+}
